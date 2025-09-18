d159d64dc4c4e97ef87857e03dfd6b00d63e5d5e
acho1990 <meskhadze90@gmail.com>
2025-09-18 14:11:44 +0400

    Homework4 V2

---
+++ Homework4_V2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Homework4_V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, enter your name! ");
+
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+
+            Console.WriteLine(Console.ReadLine());
+        }
+    }
+}
